6724bdc04abcbed3ea3d6658618b68cc5a23da7c
Kallinikos <lahmar_ahmed@yahoo.com>
2016-11-06 16:51:47 +0100

    Before UniImgLoader

--- app/src/main/java/com/kallinikos/tech/sweetdeals/adapter/RecyclerAdapter.java
+++ app/src/main/java/com/kallinikos/tech/sweetdeals/adapter/RecyclerAdapter.java
@@ -1,8 +1,66 @@
 package com.kallinikos.tech.sweetdeals.adapter;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.kallinikos.tech.sweetdeals.R;
+import com.kallinikos.tech.sweetdeals.model.Category;
+
+import java.util.List;
+
 /**
  * Created by kallinikos on 14/10/16.
  */
 
-public class RecyclerAdapter {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+    private static final String TAG = RecyclerAdapter.class.getSimpleName();
+
+    private List<Category> mData;
+    private LayoutInflater mInflater;
+
+    public RecyclerAdapter(Context context, List<Category> data){
+        this.mData = data;
+        this.mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.cat_list_item, parent, false);
+        MyViewHolder holder = new MyViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Category currentObj = mData.get(position);
+        holder.setData(currentObj);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+        ImageView imgThumb;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            title       = (TextView)  itemView.findViewById(R.id.txv_row);
+            imgThumb    = (ImageView) itemView.findViewById(R.id.img_row);
+        }
+
+        //From db
+        public void setData(Category current) {
+            this.title.setText(current.getTitle());
+            this.imgThumb.setImageResource(current.getImageId());
+        }
+    }
 }
---
+++ app/src/main/java/com/kallinikos/tech/sweetdeals/model/Category.java
@@ -0,0 +1,26 @@
+package com.kallinikos.tech.sweetdeals.model;
+
+/**
+ * Created by kallinikos on 30/10/16.
+ */
+
+public class Category {
+    private String title;
+    private int imageId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+}
